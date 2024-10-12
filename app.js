63043e10947d2fca873a17ef3bfc316426a58b16
unknown <a.normohammadi83@gmail.com>
2024-10-13 01:11:32 +0330

    create frontend and showing users

--- app.js
+++ app.js
@@ -14,7 +14,38 @@ app.use(express.static("public"));
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-    console.log(`Socket Connected.`);
+    const socketExist = activeUsers.find(
+        (socketExist) => socketExist === socket.id
+    );
+
+    if (!socketExist) {
+        activeUsers.push(socket.id);
+
+        socket.emit("update-user-list", {
+            users: activeUsers.filter(
+                (socketExist) => socketExist !== socket.id
+            ),
+        });
+
+        socket.broadcast.emit("update-user-list", { users: [socket.id] });
+    }
+
+    socket.on("call-user", (data) => {
+        socket.to(data.to).emit("call-made", {
+            offer: data.offer,
+            socket: socket.id,
+        });
+    });
+
+    socket.on("disconnect", () => {
+        activeUsers = activeUsers.filter(
+            (socketExist) => socketExist !== socket.id
+        );
+
+        socket.broadcast.emit("remove-user", {
+            socketId: socket.id,
+        });
+    });
 });
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,62 @@
-const socket = io();
+const socket = io("localhost:3000");
 
 const { RTCPeerConnection, RTCSessionDescription } = window;
 
 const peerConnection = new RTCPeerConnection();
+
+async function callUser(socketId) {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+
+    socket.emit("call-user", {
+        offer,
+        to: socketId,
+    });
+}
+
+socket.on("update-user-list", ({ users }) => {
+    const activeUserContainer = document.getElementById(
+        "active-user-container"
+    );
+
+    users.forEach((socketId) => {
+        const userExist = document.getElementById(socketId);
+
+        if (!userExist) {
+            const userContainer = document.createElement("div");
+
+            const username = document.createElement("p");
+
+            userContainer.setAttribute("class", "active-user");
+            userContainer.setAttribute("id", socketId);
+            username.setAttribute("class", "username");
+            username.innerHTML = `کاربر : ${socketId}`;
+
+            userContainer.appendChild(username);
+
+            userContainer.addEventListener("click", () => {
+                userContainer.setAttribute(
+                    "class",
+                    "active-user active-user--selected"
+                );
+                const talkingWithInfo =
+                    document.getElementById("talking-with-info");
+                talkingWithInfo.innerHTML = `صحبت با :  ${socketId}`;
+                callUser(socketId);
+            });
+
+            activeUserContainer.appendChild(userContainer);
+        }
+    });
+});
+
+socket.on("remove-user", ({ socketId }) => {
+    const user = document.getElementById(socketId);
+
+    if (user) {
+        user.remove();
+    }
+});
 
 navigator.getUserMedia(
     { video: true, audio: true },
